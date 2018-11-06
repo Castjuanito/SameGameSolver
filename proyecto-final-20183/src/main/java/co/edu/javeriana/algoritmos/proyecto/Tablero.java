7cc5804d00ea44e22c1e9f173d35ca63215cab78
Juan Felipe Castañeda <juanario1997@gmail.com>
2018-11-06 09:29:40 -0500

    Add files via upload

---
+++ proyecto-final-20183/src/main/java/co/edu/javeriana/algoritmos/proyecto/Tablero.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package co.edu.javeriana.algoritmos.proyecto;
+
+/**
+ * La implementación que se utilizará dentro del robot la suministraré yo, pero es buena idea
+ * que ustedes generen la suya para probar.
+ *
+ */
+public interface Tablero extends Cloneable {
+
+    /**
+     * Efectúa una jugada sobre el tablero.
+     * 
+     * @param jugada La jugada a realizar
+     * @return puntaje de la jugada
+     * @throws IllegalArgumentException si la jugada no es válida
+     */
+    int efectuarJugada( Casilla jugada ) throws IllegalArgumentException;
+    
+    int getNumeroColores();
+    
+    int getFilas();
+    
+    int getColumnas();
+    
+}
